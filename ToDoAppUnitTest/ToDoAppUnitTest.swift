2b759fff795376fa27ea46e83dfa9ad91d2c7653
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2023-03-03 09:40:27 +0100

    test added

--- ToDoAppUnitTest/ToDoAppUnitTest.swift
+++ ToDoAppUnitTest/ToDoAppUnitTest.swift
@@ -6,30 +6,39 @@
 //
 
 import XCTest
+import CoreData
+import SwiftUI
+@testable import ToDoApp
 
 final class ToDoAppUnitTest: XCTestCase {
+    
+    let viewContext = PersistenceController(inMemory: true).container.viewContext
+    @ObservedObject var vm = TaskViewModel()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var task: Task!
+    
+    func testAddTask() throws {
+        let taskTitle = "Test Task"
+        let taskColor = "Yellow"
+        let taskDeadline = Date()
+        let taskType = "Basic"
+        let result = vm.addTask(context: viewContext)
+        
+        vm.taskTitle = taskTitle
+        vm.taskColor = taskColor
+        vm.taskDeadline = taskDeadline
+        vm.taskType = taskType
+        XCTAssertTrue(result)
+        
+        let request: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        let tasks = try viewContext.fetch(request)
+        XCTAssertEqual(tasks.count, 1)
+        
+        let task = tasks.first!
+        XCTAssertEqual(task.color, taskColor)
+        XCTAssertEqual(task.title, taskTitle)
+        XCTAssertEqual(task.deadline, taskDeadline)
+        XCTAssertEqual(task.type, taskType)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
